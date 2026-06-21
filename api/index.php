33c8fc8a35fdbb5ec33e1181c63bdbe20d2fbffe
fitrinyc <novyanti.fitria04@gmail.com>
2026-06-21 21:06:15 +0700

    fix: setup writable tmp dirs for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
+$dirs = [
+    '/tmp/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/testing',
+    '/tmp/storage/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
